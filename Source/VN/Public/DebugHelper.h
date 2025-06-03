db36cb92616aff0578bf1cbb8dcbc756a93b986d
JinWoo <cheong546@gmail.com>
2025-06-03 21:06:44 +0900

    feat: 디버그 헬퍼 파일 추가

---
+++ Source/VN/Public/DebugHelper.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+namespace DebugHelper
+{
+	static void Print(const FString& Message, const FColor& Color = FColor::MakeRandomColor(), const int32 InKey = -1)
+	{
+		if (GEngine)
+		{
+			GEngine->AddOnScreenDebugMessage(InKey, 7.f, Color, Message);
+			UE_LOG(LogTemp, Warning, TEXT("%s"), *Message);
+		}
+	}
+
+	static void Print(const FString& FloatTitle, float FloatValueToPrint, int32 InKey = -1, const FColor& Color = FColor::MakeRandomColor())
+	{
+		if (GEngine)
+		{
+			const FString FinalMsg = FloatTitle + TEXT(": ") + FString::SanitizeFloat(FloatValueToPrint);
+			GEngine->AddOnScreenDebugMessage(InKey, 7.f, Color, FinalMsg);
+			UE_LOG(LogTemp, Warning, TEXT("%s"), *FinalMsg);
+		}
+	}
+}
