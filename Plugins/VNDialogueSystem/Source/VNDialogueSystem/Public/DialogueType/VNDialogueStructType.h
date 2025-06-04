9991165a92895ac3511b6d54c3b3cbe0497d2533
JinWoo <cheong546@gmail.com>
2025-06-04 21:00:57 +0900

    feat: 다이얼로그 플러그인에서 구조체 정의할 헤더 파일 추가

---
+++ Plugins/VNDialogueSystem/Source/VNDialogueSystem/Public/DialogueType/VNDialogueStructType.h
@@ -0,0 +1,49 @@
+﻿// Copyright © 2025 Choi-JinWoo. All Rights Reserved.
+
+#pragma once
+#include "VNDialogueEnumType.h"
+#include "VNDialogueStructType.generated.h"
+
+
+USTRUCT(BlueprintType)
+struct FCharacterSetting
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(BlueprintReadOnly)
+	FText CharacterName;
+	
+	UPROPERTY(BlueprintReadOnly)
+	EDialoguePosition DialoguePosition;
+
+	UPROPERTY(BlueprintReadOnly)
+	UTexture2D* CharacterImage;
+
+	UPROPERTY(BlueprintReadOnly)
+	bool bIsSpeaking;
+	
+};
+
+USTRUCT(BlueprintType)
+struct FTextDialogueInfo
+{
+	GENERATED_BODY()
+
+	UPROPERTY(BlueprintReadOnly)
+	TArray<FCharacterSetting> CharacterSettings;
+	
+	UPROPERTY(BlueprintReadOnly)
+	FText DialogueText;
+
+	UPROPERTY(BlueprintReadOnly)
+	TSoftObjectPtr<USoundBase> BGM;
+
+	UPROPERTY(BlueprintReadOnly)
+	TSoftObjectPtr<UTexture2D> BGImage;
+
+	UPROPERTY(BlueprintReadOnly)
+	TSoftObjectPtr<UTexture2D> CutScene;
+	
+	FName NextNodeId;
+	
+};
