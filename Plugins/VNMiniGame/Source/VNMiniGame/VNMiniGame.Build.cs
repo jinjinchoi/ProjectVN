eb9e3fdddf5ca9ffef30fa806d6d63f3395b40d8
JinWoo <cheong546@gmail.com>
2025-06-20 13:43:29 +0900

    deps: 모션 워핑 컴포넌트 의존성 추가

--- Plugins/VNMiniGame/Source/VNMiniGame/VNMiniGame.Build.cs
+++ Plugins/VNMiniGame/Source/VNMiniGame/VNMiniGame.Build.cs
@@ -39,6 +39,7 @@ public class VNMiniGame : ModuleRules
 				"Engine",
 				"InputCore",
 				"EnhancedInput",
+				"MotionWarping"
 				// ... add private dependencies that you statically link with here ...	
 			}
 			);
